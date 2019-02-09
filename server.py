a3999d6e4268cad724fe860b92daed11ded80146
jribom <jribom@kth.se>
2019-02-09 17:59:05 +0100

    :bug: Set correct host on server creation

--- server.py
+++ server.py
@@ -46,4 +46,4 @@ def user(username):
                            comparison_list = reader.get_likeness(name))
 
 if __name__ == '__main__':
-    app.run(port = PORT)
+    app.run(host = '0.0.0.0', port = PORT)
