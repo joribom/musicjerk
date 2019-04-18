ac2d3dc238039637e298aaa4a8dd3ca02fed8b24
jribom <jribom@kth.se>
2019-04-18 18:50:19 +0200

    :construction: Move database modules to separate package

--- server/api.py
+++ server/api.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, jsonify, request
 import subprocess
 import urllib.parse
-from . import dbreader
+from .dbutil import dbreader
 
 api_blueprint = Blueprint('api', __name__)
 
