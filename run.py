ffba83d73cb3b4c83e8bbfbde140b15bb72caec4
jribom <jribom@kth.se>
2019-04-18 18:32:32 +0200

    :white_check_mark: Make run.py follow PEP 8

--- run.py
+++ run.py
@@ -1,13 +1,10 @@
 import os
-import subprocess
 from flask import Flask
 from werkzeug.serving import run_simple
 from server.views import musicjerk_blueprint
 from server.api import api_blueprint
 
 PORT = 8000
-
-
 fullchain = '/etc/letsencrypt/live/bigmusicjerk.com/fullchain.pem'
 privkey = '/etc/letsencrypt/live/bigmusicjerk.com/privkey.pem'
 debug = not (os.path.exists(fullchain) and os.path.exists(privkey))
@@ -19,7 +16,6 @@ app = Flask(
 )
 app.config.from_object('configurations.DevelopmentConfig')
 
-
 # register the blueprints
 app.register_blueprint(musicjerk_blueprint)
 app.register_blueprint(api_blueprint)
@@ -29,7 +25,17 @@ if __name__ == '__main__':
     app.config['TEMPLATES_AUTO_RELOAD'] = False
     if debug:
         print('Running in debug mode! (http://localhost:8000/)')
-        app.run('localhost', PORT, debug=True)
+        app.run(
+            'localhost',
+            PORT,
+            debug=True
+        )
     else:
         print('Running in server mode! (DEBUG == off)')
-        run_simple('0.0.0.0', PORT, app, use_reloader=True, ssl_context=(fullchain, privkey))
+        run_simple(
+            '0.0.0.0',
+            PORT,
+            app,
+            use_reloader=True,
+            ssl_context=(fullchain, privkey)
+        )
